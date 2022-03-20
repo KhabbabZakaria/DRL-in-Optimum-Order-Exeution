b5d99763aa37b9ff7d7edf3436145781e7f5e0ec
KhabbabZakaria <46716277+KhabbabZakaria@users.noreply.github.com>
2022-03-21 05:19:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def calc_twap(df_csv):
 
 ref_file = files[ref_stock]
 df = pd.read_csv(os.path.join(ref_folder, ref_file))
-df = df.iloc[T*30:-T*100]
+#df = ... select the training days
 twap_price = []
 for i in range(int(len(df)/T)):
     price = calc_twap(df.iloc[i*T:(i+1)*T])
@@ -115,8 +115,6 @@ def train():
             env.reset()
             state, price_time_plus_1 = getState(all_train_list[day], all_train_vwap_list[day], env.time, state_size)
             for current_time in range(state_size, T-1):
-                #left_target = [Q-x for x in env.already_bought_list]
-                #left_target_tensor = torch.Tensor(left_target)
                 already_bought_tensor = torch.Tensor(env.already_bought_list)
                 left_time = T - env.time
                 left_time_tensor = torch.Tensor(list(range(left_time+1,left_time+1+state_size)))
@@ -128,15 +126,11 @@ def train():
                 Value = Value.to(device)
                 inference_state = model.inference_state
                 inference_state = inference_state.to(device)
-                reward = (price_time_plus_1/price_bar[day, env.time] - 1)*action - action**2 #R+ - R-
+                reward = #calculate reward
                 P_bar_strategy = P_bar_strategy + action*price_time_plus_1
-
 
                 env.step(action)
                 next_state, price_time_plus_2 =  getState(all_train_list[day], all_train_vwap_list[day], env.time, state_size)
-
-                #left_target = [Q-x for x in env.already_bought_list]
-                #left_target_tensor = torch.Tensor(left_target)
                 already_bought_tensor = torch.Tensor(env.already_bought_list)
                 left_time = T - env.time
                 left_time_tensor = torch.Tensor(list(range(left_time+1,left_time+1+state_size)))
@@ -147,28 +141,8 @@ def train():
                 _, next_Value = agent.act(private_input, next_state)
 
                 A_hat = reward + gamma*next_Value - Value
-                pdf_prob_now = find_pdf(model.policy, action, inference_state)
-                pdf_prob_old = find_pdf(policy_old, action, inference_state)
-
-                mu1, sigma1, _ = model.policy(inference_state)
-                mu2, sigma2, _ = policy_old(inference_state)
-                tup1 = (mu1, sigma1)
-                tup2 = (mu2, sigma2)
-
-                kl = KLDiv(tup1, tup2)
-                #print(kl)
-                d = (pdf_prob_now/pdf_prob_old)*A_hat
-                Lp = -(d - beta*kl)
-                policy_losses.append(Lp)
-                if Lp<0:
-                    print(Lp, (pdf_prob_now/pdf_prob_old), A_hat, kl, reward, next_Value, Value)
-                Vt = gamma*next_Value.to(device) + reward.to(device)
-                #Lv = F.smooth_l1_loss(Vt, Value)
-                Lv = Vt - Value
-                value_losses.append(Lv)
+                #calculation of Policy and  Value Losses
                 #print('Lv', Lv)
-                Ld = 0 #need to adjust............................................................................................
-                Distillation_losses.append(torch.Tensor(Ld))
                 state = next_state
                 price_time_plus_1 = price_time_plus_2
                 if left_time == 1 and env.already_bought>0:    #no more time left yet target left
@@ -188,31 +162,16 @@ def train():
             if day == len(all_train_list):
                 with torch.no_grad():
                     policy_old = copy.deepcopy(model.policy)
-            print(torch.stack(policy_losses).mean().to(device) , alpha*torch.stack(value_losses).mean().to(device))
-            #loss = torch.stack(policy_losses).mean().to(device) + alpha*torch.stack(value_losses).mean().to(device) + mu*torch.stack(Distillation_losses).mean().to(device)
-            loss = torch.stack(policy_losses).mean().to(device) + alpha*torch.stack(value_losses).mean().to(device) - PA
+           
+            loss = torch.stack(policy_losses).mean().to(device) + alpha*torch.stack(value_losses).mean().to(device)
             text_file.write(str(current_time) + ',' + str(ep) + ',' + str(day) + ',' + str(torch.stack(policy_losses).mean().item()) + ',' + str(alpha*torch.stack(value_losses).mean().item()) + ',' + str(loss.item()))
             text_file.write('\n')
             text_file.close()
-            print('loss',loss.item())
-            #writer.add_scalar('Loss/train', loss.cpu().detach().numpy(), day)
 
             loss.backward()
             optimizer.step()
 
             day = day + 1
-
-            if torch.stack(policy_losses).mean().to(device) >= 0:
-                torch.save(model.state_dict(), 'seed_'+ str(seed) + '_' + PATH)
-            else:
-                break
-
-        
-        if torch.stack(policy_losses).mean().to(device) >= 0:
-            torch.save(model.state_dict(), 'seed_'+ str(seed) + '_' + PATH)
-        else:
-            break
-
 
 
     plot1 = plt.figure(1)
